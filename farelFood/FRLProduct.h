587d6b515ad67e28e9b58276e5fa4874229ca389
crestana <xf86cfg@gmail.com>
2014-03-14 13:58:13 +0400

    Product added

---
+++ farelFood/FRLProduct.h
@@ -0,0 +1,21 @@
+//
+//  FRLProduct.h
+//  farelFood
+//
+//  Created by crestana on 14/03/14.
+//  Copyright (c) 2014 farel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FRLProduct : NSObject <NSCoding>
+
+@property (nonatomic, strong) NSString *name;
+@property (nonatomic, strong) NSString *description;
+@property (nonatomic, strong) NSMutableSet *tags;
+
+- (instancetype) initWithName:(NSString *)name andDescription:(NSString *)description andTags:(NSMutableSet *)tags;
+- (void)addTag:(NSString *)tag;
+- (void)removeTag:(NSString *)tag;
+
+@end
---
+++ farelFood/FRLProduct.m
@@ -0,0 +1,64 @@
+//
+//  FRLProduct.m
+//  farelFood
+//
+//  Created by crestana on 14/03/14.
+//  Copyright (c) 2014 farel. All rights reserved.
+//
+
+#import "FRLProduct.h"
+
+@implementation FRLProduct
+
+- (id)initWithCoder:(NSCoder *)decoder
+{
+    self = [super init];
+    if (!self) {
+        return nil;
+    }
+    
+    self.name = [decoder decodeObjectForKey:@"name"];
+    self.description = [decoder decodeObjectForKey:@"description"];
+    self.tags = [decoder decodeObjectForKey:@"tags"];
+    
+    return self;
+}
+
+- (void)encodeWithCoder:(NSCoder *)encoder
+{
+    [encoder encodeObject:self.name forKey:@"name"];
+    [encoder encodeObject:self.description forKey:@"description"];
+    [encoder encodeObject: self.tags forKey:@"tags"];
+}
+
+- (instancetype)initWithName:(NSString *)name andDescription:(NSString *)description andTags:(NSMutableSet *)tags
+{
+    self = [super init];
+    if (self) {
+        self.name = name;
+        self.description = description;
+        self.tags = tags;
+    }
+    return self;
+}
+
+- (NSMutableSet *)tags
+{
+    if (!_tags) {
+        _tags = [[NSMutableSet alloc] init];
+    }
+    return _tags;
+}
+
+- (void)addTag:(NSString *)tag
+{
+    [self.tags addObject:tag];
+}
+
+- (void)removeTag:(NSString *)tag
+{
+    [self.tags removeObject:tag];
+}
+
+
+@end
