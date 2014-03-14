4ff0a034e5a62968974d73e90ce8c6455e66472c
crestana <xf86cfg@gmail.com>
2014-03-14 13:03:54 +0400

    init

---
+++ farelFood/FRLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FRLAppDelegate.h
+//  farelFood
+//
+//  Created by crestana on 14/03/14.
+//  Copyright (c) 2014 farel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FRLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ farelFood/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  farelFood
+//
+//  Created by crestana on 14/03/14.
+//  Copyright (c) 2014 farel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FRLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FRLAppDelegate class]));
+    }
+}
