ee377f6b975bbaeb1a1482320eb891fc08b92832
crestana <xf86cfg@gmail.com>
2014-03-19 12:26:16 +0400

    XML for main categories added

--- farelFood/FRLProducts.h
+++ farelFood/FRLProducts.h
@@ -9,7 +9,7 @@
 #import <Foundation/Foundation.h>
 
 @interface FRLProducts : NSObject
-@property (nonatomic, strong, readonly) NSMutableArray *products;
+//@property (nonatomic, strong, readonly) NSMutableArray *products;
 
 + (FRLProducts *)sharedDataBase;
 
