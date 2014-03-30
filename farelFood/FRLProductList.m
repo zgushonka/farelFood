48885ad42e517f88825d27ffdbc299d8d09b0fff
oleksandr.buravlyov <oleksandr.buravlyov@globallogic.com>
2014-03-30 15:25:55 +0300

    code refactoring; some files want to be attached

--- farelFood/FRLProductList.m
+++ farelFood/FRLProductList.m
@@ -11,9 +11,6 @@
 #import "FRLProduct.h"
 #import "FRLProductDetailed.h"
 
-@interface FRLProductList ()
-
-@end
 
 @implementation FRLProductList
 
@@ -23,18 +20,7 @@
     [self.tableView registerNib:[UINib nibWithNibName:@"FRLTableViewCell" bundle:nil] forCellReuseIdentifier:@"ProductIdentifier"];
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-}
-
 #pragma mark - Table view data source
-
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
-{
-    return 1;
-}
-
 - (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
 {
     return @"Tester";
@@ -46,10 +32,8 @@
     NSLog (@"There are: %d", [self.productsToDisplay count]);
 }
 
-
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-
     FRLTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ProductIdentifier"];
     if (cell == nil) {
         cell = [[FRLTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"ProductIdentifier"];
@@ -58,30 +42,29 @@
     FRLProduct *currentProduct = [self.productsToDisplay objectAtIndex:indexPath.row];
     
     NSAttributedString *currentProductAttributedName = [[NSAttributedString alloc] initWithString:currentProduct.name attributes:@{ NSFontAttributeName : [UIFont preferredFontForTextStyle:UIFontTextStyleBody]}];
-    
+    [cell.name setAttributedText:currentProductAttributedName];
     
     NSAttributedString *currentProductAttributedDescription = [[NSAttributedString alloc] initWithString:currentProduct.description attributes:@{ NSFontAttributeName : [UIFont preferredFontForTextStyle:UIFontTextStyleCaption1], NSForegroundColorAttributeName : [UIColor grayColor]}];
-    
-    [cell.name setAttributedText:currentProductAttributedName];
     [cell.description setAttributedText:currentProductAttributedDescription];
+    
     [cell.preview setImage:[UIImage imageNamed:currentProduct.image]];
-    
-    
-    
     return cell;
 }
 
 #pragma mark - Table view delegate
-
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
     FRLProduct *selectedProduct = [self.productsToDisplay objectAtIndex:indexPath.row];
+    [self showDetailsForProduct:selectedProduct];
+}
+
+- (void)showDetailsForProduct:(FRLProduct *)product
+{
     FRLProductDetailed *detailedProductController = [[FRLProductDetailed alloc] init];
-    
-    detailedProductController.product = selectedProduct;
-    detailedProductController.title = selectedProduct.name;
-    
+    detailedProductController.product = product;
+    detailedProductController.title = product.name;    
     [self.navigationController pushViewController:detailedProductController animated:YES];
 }
+
 
 @end
--- farelFood/FRLTableAllCategories.m
+++ farelFood/FRLTableAllCategories.m
@@ -13,9 +13,6 @@
 #import "FRLProducts.h"
 #import "FRLProductList.h"
 
-@interface FRLTableAllCategories ()
-
-@end
 
 @implementation FRLTableAllCategories
 
@@ -24,26 +21,15 @@
     [super viewDidLoad];
     
     [self.tableView registerNib:[UINib nibWithNibName:@"FRLTableViewCell" bundle:nil] forCellReuseIdentifier:@"CategoryIdentifier"];
+    [self.tableView setSeparatorInset:UIEdgeInsetsMake(0, 0, 0, 0)];
     
   //!-- REWRITE THIS
-    UISearchBar *new = [[UISearchBar alloc] initWithFrame:CGRectMake(0, 0, 420, 30)];
+    UISearchBar *new = [[UISearchBar alloc] initWithFrame:CGRectMake(0, 0, 320, 30)];
     self.tableView.tableHeaderView = new;
   //----!
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-}
-
 #pragma mark - Table view data source
-
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
-{
-    return 1;
-}
-
-
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
     return [self.mainCategoriesDatabase count];
@@ -52,7 +38,6 @@
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
- 
     FRLTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"CategoryIdentifier"];
     if (cell == nil) {
         cell = [[FRLTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"CategoryIdentifier"];
@@ -61,28 +46,28 @@
     FRLMainCategory *currentCategory = [self.mainCategoriesDatabase categoryAtIndex:indexPath.row];
     
     NSAttributedString *currentCategoryAttributedName = [[NSAttributedString alloc] initWithString:currentCategory.name attributes:@{ NSFontAttributeName : [UIFont preferredFontForTextStyle:UIFontTextStyleBody]}];
+    [cell.name setAttributedText:currentCategoryAttributedName];
     
     NSAttributedString *currentCategoryAttributedDescription = [[NSAttributedString alloc] initWithString:currentCategory.description attributes:@{ NSFontAttributeName : [UIFont preferredFontForTextStyle:UIFontTextStyleCaption1], NSForegroundColorAttributeName : [UIColor grayColor]}];
-    
-    [cell.name setAttributedText:currentCategoryAttributedName];
     [cell.description setAttributedText:currentCategoryAttributedDescription];
-    [cell.preview setImage:[UIImage imageNamed:currentCategory.image]];
     
+    [cell.preview setImage:[UIImage imageNamed:currentCategory.image]];    
     return cell;
-    
 }
-
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
     FRLMainCategory *selectedCategory = [self.mainCategoriesDatabase categoryAtIndex:indexPath.row];
-    NSArray *foundProducts = [self.productsDatabase productsConformingTags:selectedCategory.tags];
-    
-    FRLProductList *foundProductsController = [[FRLProductList alloc] init];
-    foundProductsController.productsToDisplay = foundProducts;
-    foundProductsController.title = selectedCategory.name;
-    
-    [self.navigationController pushViewController:foundProductsController animated:YES];
+    [self showControllerWithCategory:selectedCategory];
+}
+
+- (void)showControllerWithCategory:(FRLMainCategory *)category
+{
+    NSArray *productsInSelectedCategory = [self.productsDatabase productsConformingTags:category.tags];
+    FRLProductList *productsController = [[FRLProductList alloc] init];
+    productsController.productsToDisplay = productsInSelectedCategory;
+    productsController.title = category.name;    
+    [self.navigationController pushViewController:productsController animated:YES];
 }
 
 @end
