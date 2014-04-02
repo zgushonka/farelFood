e4a98641784a100f16127cb6e28c5b1f3ffe2485
oleksandr.buravlyov <oleksandr.buravlyov@globallogic.com>
2014-04-02 17:48:08 +0300

    add storyboard and new AllCategories VC

---
+++ farelFood/AllCategoriesVC.h
@@ -0,0 +1,13 @@
+//
+//  AllCategoriesVC.h
+//  farelFood
+//
+//  Created by oleksandr.buravlyov on 4/2/14.
+//  Copyright (c) 2014 farel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AllCategoriesVC : UIViewController
+
+@end
